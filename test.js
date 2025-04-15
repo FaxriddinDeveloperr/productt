a0d2dca4cf245bc3d4f068cba84be22ce0bc8470
FaxriddinDeveloperr <fazliddinmariipov@gmail.com>
2025-04-15 12:29:49 +0500

    sasa

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ const hashedPassword2 = await bcrypt.hash(password, salt);
 const hashedPassword3 = await bcrypt.hash(password, salt);
 
 const newPassword = "qwer12345";
-console.log(hashedPassword); //dalsndlnasldnlkasndlnaslknd
-console.log(hashedPassword2); //dalsndlnasldnlkasndlnaslknd
-console.log(hashedPassword3); //dalsndlnasldnlkasndlnaslknd
+console.log(hashedPassword);
+console.log(hashedPassword2);
+console.log(hashedPassword3);
