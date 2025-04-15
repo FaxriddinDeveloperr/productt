2c994fa0a93397dac1f16b596807403aa813cf63
FaxriddinDeveloperr <fazliddinmariipov@gmail.com>
2025-04-15 12:52:31 +0500

    y

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,8 +32,7 @@ userSchema.pre("save", async function (next) {
   try {
     if (!this.isModified("password")) return next();
 
-    // const salt = await bcrypt.genSalt(10);
-    // this.password = await bcrypt.hash(this.password, salt);
+  
     this.password = this.password;
 
     next();
